d9b2d93d28643197c0ef276e9493e3c9672943e6
Oleg Klimov <oleg.football.klimov@gmail.com>
2024-06-30 19:48:31 +0300

    Add suits and ranks of cards

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace CardGame
+public enum Suit
 {
-    internal class Program
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades
+}
+public enum Rank
+{
+    Ace = 11,
+    King = 4,
+    Queen = 3,
+    Jack = 2,
+    Ten = 10,
+    Nine = 9,
+    Eight = 8,
+    Seven = 7,
+    Six = 6
+}
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
